a97ae2bf3f1e08ce46784135bbeb3f0afdd705fa
Sidiki Kaba <sidiki.kaba@disinstruments.fr>
2022-06-01 09:52:42 +0200

    ajout de routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,22 @@
 import express from "express";
-const router = express.Router();
-
 import InscriptionController from "../controllers/inscription.js";
 import LoginController from "../controllers/login.js";
 import LogoutController from "../controllers/logout.js";
+import AboutController from "../controllers/about.js";
+import {
+  CreateUserController,
+  LoginUserController,
+} from "../controllers/user.js";
+import DashboardController from "../controllers/dashboard.js";
+import { authMiddleware } from "../middlewares.js";
 
-router.get("/", InscriptionController);
+const router = express.Router();
+router.get("/inscription", InscriptionController);
 router.get("/login", LoginController);
+router.get("/", AboutController);
 router.get("/logout", LogoutController);
+router.get("/dashboard", authMiddleware, DashboardController);
 
+router.post("/inscription", CreateUserController);
+router.post("/login", LoginUserController);
 export default router;
